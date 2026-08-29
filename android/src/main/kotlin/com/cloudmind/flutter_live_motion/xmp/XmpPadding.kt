23327d461033425cedd62adb008b962e81b821d4
wanzq <13775817104@163.com>
2026-08-29 15:17:15 +0800

    Add android/src/main/kotlin/com/cloudmind/flutter_live_motion/xmp/XmpPadding.kt

---
+++ android/src/main/kotlin/com/cloudmind/flutter_live_motion/xmp/XmpPadding.kt
@@ -0,0 +1,15 @@
+package com.cloudmind.flutter_live_motion.xmp
+
+object XmpPadding {
+    // ~2KB of whitespace padding to ensure XMP packet is editable and compliant with strict parsers
+    val PADDING = "                                                                                                    " +
+            "                                                                                                    " +
+            "                                                                                                    " +
+            "                                                                                                    " +
+            "                                                                                                    " +
+            "                                                                                                    " +
+            "                                                                                                    " +
+            "                                                                                                    " +
+            "                                                                                                    " +
+            "                                                                                                    "
+}
